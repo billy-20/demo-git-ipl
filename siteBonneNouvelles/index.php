fe721768fb7a73303ad9b0f950469bb06a6dfc95
Bilal El Haddadi <bilal.elhaddadi@student.vinci.be>
2020-10-02 17:48:56 +0200

    add - newFuntion (livresController)

--- siteBonneNouvelles/index.php
+++ siteBonneNouvelles/index.php
@@ -32,14 +32,23 @@ function geneseController()
     return $controller;
 }
 
+/**
+ * @return LivresController
+ */
+function livresController()
+{
+    require_once('controllers/LivresController.php');
+    $controller = new LivresController();
+    return $controller;
+}
+
 switch($action) {
 		case 'genese':
 			$controller = geneseController();
             break;
 		case 'livres':
-			require_once('controllers/LivresController.php');	
-			$controller = new LivresController();
-			break;
+			$controller = livresController();
+            break;
 		case 'contact':
 			require_once('controllers/ContactController.php');	
 			$controller = new ContactController();
